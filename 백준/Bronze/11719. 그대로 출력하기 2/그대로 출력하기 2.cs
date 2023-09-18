68e7df67134c8342ab051b841a1ded8dba8006e8
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-09-18 10:44:08 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 64 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Solution
+{
+    public static void Main()
+    {
+        string s;
+        while ((s = Console.ReadLine()) != null)
+        {
+             
+            Console.WriteLine(s);
+        }
+    }
+}
