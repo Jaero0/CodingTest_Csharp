4c3229ccf4f5555c043fc8d6fa569af4c16e5b67
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-09-25 10:50:00 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.37 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using System;
+
+public class Solution
+{
+    public string solution(string X, string Y)
+    {
+        string answer = "";
+        int[] a = new int[10]; //0~9 자연수 갯수 담을 최빈값 알고리즘 배열
+        int[] b = new int[10];
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            a[(int)(X[i] - 48)]++;
+        }
+        for (int i = 0; i < Y.Length; i++)
+        {
+            b[(int)(Y[i] - 48)]++;
+        }
+
+        StringBuilder pair = new StringBuilder();
+
+        for (int i = 9; i >= 0; i--) //자동으로 숫자 갯수만큼 정렬되어있으니, 
+        {
+            while (a[i] > 0 && b[i] > 0) //두 배열안에 같은 값이 들어있을때 pair에 합쳐줌
+            {
+                pair.Append(i);
+                a[i]--;
+                b[i]--;
+            }
+        }
+
+        if (string.IsNullOrEmpty(pair.ToString()))
+        {
+            return "-1";
+        }
+        else if("0".Equals(pair.ToString().Substring(0, 1))) // 첫 시작이 0일때,
+        {
+            return "0";
+        }
+        else
+        {
+            return pair.ToString(); 
+        }
+    }
+}
