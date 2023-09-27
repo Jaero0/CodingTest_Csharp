c61ea511883dac6182a09fc52e1dff8bf6c2808d
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-09-27 09:31:56 +0900

    [unrated] Title: 문자열 나누기, Time: 0.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+
+        int firstCount = 0;
+        int diffCount = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char first = s[i];
+
+            for (int j = i; j < s.Length; j++)
+            {
+                if (s[j] == first)
+                {
+                    firstCount++;
+                }
+                else
+                {
+                    diffCount++;
+                }
+
+                if (firstCount == diffCount)
+                {
+                    firstCount = 0;
+                    diffCount = 0;
+                    answer++;
+                    i = j;
+                    break;
+                }
+            }
+        }
+
+        if (firstCount != diffCount)
+        {
+            return answer + 1;
+        }
+        return answer;
+    }
+}
