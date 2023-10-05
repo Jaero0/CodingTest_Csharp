a7320b55aa787542d9075d91c8fb76ff6bc2da70
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-10-06 08:42:30 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 public class Solution
 {
@@ -10,77 +6,26 @@ public class Solution
     {
         int[] answer = new int[2];
 
-        int min = 0;
-        int max = 0;
-
-        if (n > m)
-        {
-            for (int i = 1; i < n; i++)
-            {
-                if (n % i == 0 && m % i == 0)
-                {
-                    if (i > max)
-                    {
-                        max = i;
-                    }
-
-                }
-                else if (i > m)
-                {
-                    break;
-                }
-            }
-
-            int asd = m;
-
-            while (true)
-            {
-
-                if (asd % n == 0 && asd % m == 0)
-                {
-                    min = asd;
-                    break;
-                }
-                asd++;
-            }
-        }
-
-        else if (n < m)
-        {
-            for (int i = 1; i < m; i++)
-            {
-                if (n % i == 0 && m % i == 0)
-                {
-                    if (i > max)
-                    {
-                        max = i;
-                    }
-
-                }
-                else if (i > n)
-                {
-                    break;
-                }
-            }
-
-            int asd = m;
-
-            while (true)
-            {
-
-                if (asd % n == 0 && asd % m == 0)
-                {
-                    min = asd;
-                    break;
-                }
-                asd++;
-            }
-        }
-
-        answer[0] = max;
-        answer[1] = min;
-
+        answer[0] = GCD(n, m);
+        answer[1] = LCM(n*m, answer[0]);
 
         return answer;
+    }
+
+    public int GCD(int n, int m)
+    {
+        if (n % m == 0)
+        {
+            return m;
+        }
+        else
+        {
+            return GCD(m, n % m);
+        }
+    }
+
+    public int LCM(int n, int m)
+    {
+        return n / m;
     }
 }
