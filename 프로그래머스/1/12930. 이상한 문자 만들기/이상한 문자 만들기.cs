9466498bc9146be9baef6f5322b88c1afafd2918
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-10-11 09:06:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.91 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        string[] sa = s.Split();
+
+        for (int i = 0; i < sa.Length; i++)
+        {
+            char[] chars = new char[sa[i].Length];
+
+            for (int j = 0; j < sa[i].Length; j++)
+            {
+                chars[j] = sa[i][j];
+            }
+
+            for (int k = 0; k < chars.Length; k++)
+            {
+                if (k % 2 == 0)
+                {
+                    sb.Append(chars[k].ToString().ToUpper());
+                }
+                else
+                {
+                    sb.Append(chars[k].ToString().ToLower());
+                }
+            }
+            if (i == sa.Length-1)
+            {
+                break;
+            }
+            sb.Append(" ");
+        }
+        return sb.ToString();
+    }
+}
