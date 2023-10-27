d168562b2b4ef420e1a13a767dac581bc10e64a5
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-10-27 09:34:45 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,16 +3,16 @@ class Solution {
         int length = num_list.length;
         int[] answer = new int[length+1];
 
+        System.arraycopy(num_list, 0, answer, 0, length);
+        
         int finalNum = 0;
         if (num_list[length-1] >
                 num_list[length-2]) {
-            finalNum = num_list[length-1] - num_list[length-2];
+            answer[length] = num_list[length-1] - num_list[length-2];
         } else {
-            finalNum = num_list[length-1] *2;
+            answer[length] = num_list[length-1] *2;
         }
-
-        System.arraycopy(num_list, 0, answer, 0, length);
-        answer[length] = finalNum;
+        
         return answer;
     }
 }
