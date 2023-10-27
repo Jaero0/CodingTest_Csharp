51df4b0d4f05f35de771237dde758b956bfc362c
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-10-27 20:54:12 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,16 +1,20 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] input = br.readLine().split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
 
-        System.out.println(a+b);
+        bw.write(String.valueOf(a+b));
+        bw.close();
     }
 }
