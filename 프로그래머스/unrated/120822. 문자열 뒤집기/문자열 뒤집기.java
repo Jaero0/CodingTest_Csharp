3ddd123155a21ff9998dcf7d2f849a258e0a740e
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-11-16 10:14:44 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(my_string).reverse();
+        
+        return sb.toString();
+    }
+}
