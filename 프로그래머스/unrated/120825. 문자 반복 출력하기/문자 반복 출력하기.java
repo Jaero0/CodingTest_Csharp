06c24673a23e10cf22fd0ba96922a04a6c7bf84f
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-11-18 11:58:51 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 5.96 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,16 +1,29 @@
 class Solution {
     public String solution(String my_string, int n) {
-        
-        StringBuilder sb = new StringBuilder();
 
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        long time1 = System.nanoTime();
         for (char c : my_string.toCharArray()) {
 
             for (int i = 0; i < n; i++) {
-                 
+
                 sb.append(c);
             }
         }
-        
+        long ftime1 = System.nanoTime();
+        System.out.println(ftime1-time1);
+
+
+        long time2 = System.nanoTime();
+        for (char c : my_string.toCharArray()) {
+
+sb2.append((c + "").repeat((n)));
+        }
+        long ftime2 = System.nanoTime();
+        System.out.println(ftime2-time2);
+
         return sb.toString();
     }
 }
