63c319f087ba6b3c6515237d4f47ea44bdc1fd19
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-11-18 11:07:45 +0900

    [level unrated] Title: 모음 제거, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,17 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        
-        char[] c = my_string.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < c.length; i++) {
-             
-            if (c[i] == 'a' || c[i] == 'e' || c[i] == 'o' || c[i] == 'i' || c[i] == 'u') {
-                continue;
-            }
-            sb.append(c[i]);
-        }
-        
-        return sb.toString();
+        String answer = "";
+
+        answer = my_string.replaceAll("[aeiou]", "");
+
+        return answer;
     }
 }
