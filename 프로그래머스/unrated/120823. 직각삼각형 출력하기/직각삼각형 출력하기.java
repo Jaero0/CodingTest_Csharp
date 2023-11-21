46b798795c06c6a444cbcbbbcd28e47e1aff9230
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-11-21 09:29:23 +0900

    [level unrated] Title: 직각삼각형 출력하기, Time: 126.52 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+
+            for (int j = 0; j < i+1; j++) {
+
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
