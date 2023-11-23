e686772e46010fa5acef16fb6a9969d43a03ef8c
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-11-23 19:39:56 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        int i = String.valueOf(num).indexOf(String.valueOf(k));
+
+        return i != -1 ? i + 1 : i;
+    }
+}
